7779ac5f27bec1f742b8d0606a7a72858457417b
zhanglichi <zhanglichi@olesee.com>
2016-03-24 10:36:56 +0800

    init

---
+++ NeteaseSimulation/NeteaseSimulation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NeteaseSimulation
+//
+//  Created by 张立池 on 16/3/24.
+//  Copyright © 2016年 zhanglichi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ NeteaseSimulation/NeteaseSimulationTests/NeteaseSimulationTests.swift
@@ -0,0 +1,36 @@
+//
+//  NeteaseSimulationTests.swift
+//  NeteaseSimulationTests
+//
+//  Created by 张立池 on 16/3/24.
+//  Copyright © 2016年 zhanglichi. All rights reserved.
+//
+
+import XCTest
+@testable import NeteaseSimulation
+
+class NeteaseSimulationTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
